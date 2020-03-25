01968bf36862c649b49cd98a4654039fdb1ba55f
LiatB <liat.braverman@gmail.com>
2020-03-25 20:45:39 +0200

    Add files via upload

---
+++ player.js
@@ -0,0 +1,13 @@
+console.log("Hello google");
+/* var script = document.createElement('script');
+script.setAttribute('type', 'text/javascript');
+script.src = chrome.getURL('https://guidedlearning.oracle.com/player/latest/api/scenario/get/v_IlPvRLRWObwLnV5sTOaw/5szm2kaj/?callback=__5szm2kaj&refresh=true&env=dev&type=startPanel&vars%5Btype%5D=startPanel&sid=none&_=1582203987867');
+setTimeout(function() {
+	document.head.appendChild(script);
+}); */
+
+/* fetch('https://guidedlearning.oracle.com/player/latest/api/scenario/get/v_IlPvRLRWObwLnV5sTOaw/5szm2kaj/?callback=__5szm2kaj&refresh=true&env=dev&type=startPanel&vars%5Btype%5D=startPanel&sid=none&_=1582203987867')
+    .then(res => res.json())
+    .then((out) => {
+        console.log('Output: ', out);
+}).catch(err => console.error(err)); */
